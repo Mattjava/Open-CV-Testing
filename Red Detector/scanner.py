a0ebd33747c889a19be75c560b20f0e92af68d8d
Mattjava <77417435+Mattjava@users.noreply.github.com>
2024-03-28 14:55:01 -0400

    Removed unnecessary lines of code

--- Red Detector/scanner.py
+++ Red Detector/scanner.py
@@ -2,18 +2,13 @@ import cv2
 
 def detect_red(file):
     count = 0
-    try:
-        print(f"File size of {file}: {count_pixels(file)}")
-        img = cv2.imread(file, 1)
-    except(TypeError()):
-        print("File not found")
-    else:
-        for row in img:
-            for pixel in row:
-                if pixel[2] > 200 and pixel[1] < 100 and pixel[0] < 100:
-                    count += 1
-    finally:
-        print(f"{count} red pixels counted")
+    print(f"File size of {file}: {count_pixels(file)}")
+    img = cv2.imread(file, 1)
+    for row in img:
+        for pixel in row:
+            if pixel[2] > 200 and pixel[1] < 100 and pixel[0] < 100:
+                count += 1
+    print(f"{count} red pixels counted")
 
 def count_pixels(file):
     img = cv2.imread(file)
